43f0733e8c470b639aea1bdcb2480a8544cab2c9
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-08 02:41:07 -0800

    began mongodb setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://moneer:moneer1@ds041157.mlab.com:41157/devconnectordb1"
+};
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", () => res.send("Hello"));
+// Database Configuration
+const db = require("./config/keys").mongoURI;
+// Connect to DB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello"));
 
 const port = process.env.PORT || 5000;
 
